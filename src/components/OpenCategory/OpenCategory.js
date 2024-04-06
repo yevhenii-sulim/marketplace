cfcc17a6ecf0ca468b1610aaf04d455cde5909aa
yevhenii-sulim <bees127@ukr.net>
2024-04-06 15:26:31 +0300

    left error modal

--- src/components/OpenCategory/OpenCategory.js
+++ src/components/OpenCategory/OpenCategory.js
@@ -8,7 +8,6 @@ const categoryModal = document.querySelector('#category');
 
 export default memo(function OpenCategory() {
   const [toggleModal, setToggleModal] = useState(false);
-
   useEffect(() => {
     function onCloseByEsc(evt) {
       if (evt.code === 'Escape') {
@@ -21,25 +20,27 @@ export default memo(function OpenCategory() {
     };
   }, []);
 
-  function onClick(bool) {
+  function onOpen(bool) {
     setToggleModal(bool);
   }
 
   function oncloseByClickOutside(evt) {
     if (evt.currentTarget === evt.target) {
-      setToggleModal(false);
+      onOpen(false);
     }
   }
 
   return (
-    <OpenCategoryButton type="button" onClick={() => onClick(true)}>
-      <CategorySvg />
-      <TitleNav>Категорії товарів</TitleNav>
+    <>
+      <OpenCategoryButton type="button" onClick={() => onOpen(true)}>
+        <CategorySvg />
+        <TitleNav>Категорії товарів</TitleNav>
+      </OpenCategoryButton>
       {toggleModal &&
         createPortal(
           <NavComponent onCloseModal={oncloseByClickOutside} />,
           categoryModal
         )}
-    </OpenCategoryButton>
+    </>
   );
 });
