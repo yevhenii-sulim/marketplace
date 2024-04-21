cf61299511248558e11b3cc4dc2a7f22a8d53091
yevhenii-sulim <bees127@ukr.net>
2024-04-21 17:27:41 +0300

    add sing

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <PersistGate loading={null} persistor={persistor}>
       <Provider store={store}>
         <ThemeProvider theme={theme}>
-          <BrowserRouter basename="/">
+          <BrowserRouter basename="/marketplace">
             <App />
           </BrowserRouter>
         </ThemeProvider>
