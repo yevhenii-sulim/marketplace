43d8dc9505d13c512db42db5d708c31cdc7e4110
yevhenii-sulim <bees127@ukr.net>
2024-07-24 15:14:35 +0300

    add page searshed product

--- src/components/SearchedProduct.js/SearchedProduct.styled.js
+++ src/components/SearchedProduct.js/SearchedProduct.styled.js
@@ -1,27 +1,29 @@
 import styled from 'styled-components';
 
-export const Product = styled.div`
+export const Product = styled.ul`
   display: grid;
   grid-template-columns: repeat(4, auto);
   justify-content: flex-start;
   gap: 25px;
 `;
-export const ProductList = styled.ul`
-  display: flex;
-  flex-direction: column;
-  gap: 19px;
-`;
+
 export const ProductsPage = styled.div`
   display: grid;
-  grid-template-columns: 2fr 7fr;
-  column-gap: 30px;
+  grid-template-rows: auto auto;
+  justify-content: center;
+  row-gap: 30px;
   height: 100%;
+  padding-top: 30px;
+  padding-bottom: 30px;
+  header {
+    display: flex;
+    justify-content: space-between;
+  }
 `;
 export const TitleSort = styled.h2`
   font-family: 'Jost';
   font-size: 28px;
   font-weight: 400;
   line-height: 1.5;
-  margin: 0 auto;
   white-space: nowrap;
 `;
