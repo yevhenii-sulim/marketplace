32c8b977786fa5201119aaafc07702ef12c5aeff
yevhenii-sulim <bees127@ukr.net>
2024-09-10 10:52:03 +0300

    add board password

--- src/components/ChangePassword/ChangePassword.js
+++ src/components/ChangePassword/ChangePassword.js
@@ -33,7 +33,7 @@ export default function ChangePassword() {
           if (!values.new_password) {
             errors.new_password = "Обов'язкове поле";
           } else if (
-            !/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&()^{}#+._=;:'",<>/№~\-|`])[A-Za-z\d@$!%*?&()^{}#+._=;:'",<>/№~\-|]{6,}$/.test(
+            !/^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&()^{}#+._=;:'",<>/№~\-|`])[A-Za-z\d@$!%*?&()^{}#+._=;:'",<>/№~\-|]{6,20}$/.test(
               values.new_password
             )
           ) {
