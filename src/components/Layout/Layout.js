92fe850aec4462f337e0ee967ee38a54ea19e02e
yevhenii-sulim <bees127@ukr.name>
2025-02-25 20:49:22 +0200

    addd

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,7 @@
-import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Container, ContainerLayout } from './Layout.styled';
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer/Footer';
-
-function AdmitadRedirect() {
-  useEffect(() => {
-    window.location.href = "https://rzekl.com/g/1e8d114494a44cfda69c16525dc3e8/";
-  }, []);
-
-  return <p>Redirecting...</p>;
-}
 
 export default function Layout() {
   return (
@@ -19,11 +10,9 @@ export default function Layout() {
       <main>
         <Container>
           <Outlet />
-
         </Container>
       </main>
       <Footer />
-      <AdmitadRedirect />
     </ContainerLayout>
   );
 }
